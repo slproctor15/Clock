b4b51abffb27b501eea233a1acbcd43e1d1d3727
unknown <sproctor@WS-I3-24.WS-ACADBUS.EDU>
2015-05-20 10:04:30 -0700

    I like Juan.

---
+++ clokc.js
@@ -0,0 +1,21 @@
+// var d = new Date();
+// document.getElementById("demo").innerHTML = d.getDate();
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById("demo").innerHTML = h+ ":" + m + ":" + s;
+    t = setTimeout(function(){ startTime() }, 500);
+}
+
+function checkTime(i) {
+    if (i<10) {
+        i = "0" + i;
+    }
+    return i;
+}
